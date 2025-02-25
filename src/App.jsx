9ab250f8706bc5c7a333ada8cc73a5afebd82b93
Hellagr <alehkushniarou@gmail.com>
2025-02-25 15:52:39 +0300

    Switched to HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import Header from './components/Header.jsx';
 import ProfileCard from './components/ProfileCard.jsx';
